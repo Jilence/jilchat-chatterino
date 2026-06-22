f575f983577d5498bb42d00d3d6a452af42e2548
Jilence <68006348+Jilence@users.noreply.github.com>
2026-06-22 20:03:21 +0200

    Fix Kick API conflict markers

--- src/providers/kick/KickApi.cpp
+++ src/providers/kick/KickApi.cpp
@@ -297,7 +297,6 @@ void KickApi::sendMessage(uint64_t broadcasterUserID, const QString &message,
         [cb = std::move(cb)](const ExpectedStr<Response> &res) {
             if (!res)
             {
-<<<<<<< HEAD
                 cb(ExpectedStr<void>{makeUnexpected(res.error())});
                 return;
             }
@@ -308,17 +307,6 @@ void KickApi::sendMessage(uint64_t broadcasterUserID, const QString &message,
                 return;
             }
             cb(ExpectedStr<void>{});
-=======
-                cb(makeUnexpected(res.error()));
-                return;
-            }
-            if (res->isSent)
-            {
-                cb(ExpectedStr<void>{});
-                return;
-            }
-            cb(makeUnexpected(u"Message was not sent"_s));
->>>>>>> nightly-build
         });
 }
 
